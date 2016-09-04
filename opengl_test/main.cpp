7ab17100d06a3b95cb49ab01b06645486cb74b5e
Alexander-Andrade <veimar.94@gmail.com>
2016-09-04 17:11:25 +0300

    add application singleton

--- opengl_test/main.cpp
+++ opengl_test/main.cpp
@@ -3,18 +3,47 @@
 
 using namespace std;
 
-void display() {
-	glClear(GL_COLOR_BUFFER_BIT);	//clear color buffer
-	//draw white poligon
-	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_POLYGON);
-	glVertex3f(0.25, 0.25, 0.0);
-	glVertex3f(0.75, 0.25, 0.0);
-	glVertex3f(0.25, 0.75, 0.0);
-	glVertex3f(0.25, 0.75, 0.0);
-	glEnd();
+//singleton
+class Application {
+private:
+	static Application* pApplication;
+	Application() {}
+	Application(const Application&);
+	Application& operator=(Application&);
+public:
+	static Application* getApplication() {
+		if (!pApplication) 
+			pApplication = new Application();
+		return pApplication;
+	}
 
-	glFlush();	//
+	void display() {
+		glClear(GL_COLOR_BUFFER_BIT);	//clear color buffer
+		//draw white poligon
+		//glRotatef(60, 1.0, 0.0, 1.0);
+		/*glEnable(GL_LINE_SMOOTH);
+		glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);*/
+		glColor3f(1.0, 1.0, 1.0);
+		glBegin(GL_LINES);
+		glVertex3f(0.1, 0.25, 0.0);
+		glVertex3f(0.3, 0.9, 0.0);
+		glEnd();
+
+		glColor3f(1.0, 1.0, 1.0);
+		glBegin(GL_POLYGON);
+		glVertex3f(0.25, 0.25, 0.0);
+		glVertex3f(0.75, 0.25, 0.0);
+		glVertex3f(0.25, 0.75, 0.0);
+		glVertex3f(0.25, 0.75, 0.0);
+		glEnd();
+
+		glFlush();	//
+	}
+};
+Application* Application::pApplication = nullptr;
+
+void display() {
+	Application::getApplication()->display();
 } 
 
 void init() {
